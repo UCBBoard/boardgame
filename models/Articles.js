c692c2f1937abb5cf976a76f4e0bca9aaf775849
ArbutusNate <arbutusnate@gmail.com>
2017-09-23 13:22:33 -0700

    fixing models, putting Articles back in there

---
+++ models/Articles.js
@@ -0,0 +1,31 @@
+// Require mongoose
+var mongoose = require("mongoose");
+// Create Schema class
+var Schema = mongoose.Schema;
+
+// Create article schema
+var ArticleSchema = new Schema({
+  // title is a required string
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  // link is a required string
+  link: {
+    type: String,
+    required: true
+  }
+
+  // date: {
+  //   type: Date,
+  //   required: true
+  // }
+
+});
+
+// Create the Article model with the ArticleSchema
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the model
+module.exports = Article;
