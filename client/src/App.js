0ce2f10e29afef3bc72fd2dd219a85b907247e2c
Jon Berry <jonpber@gmail.com>
2017-09-23 16:09:50 -0700

    trying to get default splash page running

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
-import Gamelist from "./components/Gamelist";
-import Friendslist from "./components/Friendslist";
-import Newsfeed from "./components/Newsfeed";
 import Dashboard from "./components/Dashboard";
+import Splash from "./components/Splash";
 
 const App = () => 
   <Router>
     <Switch>
-      <Route exact path="dash" component={Dashboard} />
+      <Route exact path="/dash" component={Dashboard} />
       <Route path="/" component={Splash} />
     </Switch>
   </Router>
