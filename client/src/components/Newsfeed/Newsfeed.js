b58e4843362eeb696d3e1e8987d7c5b81fdf9afe
Jon Berry <jonpber@gmail.com>
2017-09-23 07:37:47 -0700

    added axiom, cheerio, and request to the project package.json. Newsfeed scrape currently working to scrape headlines from reddit.

--- client/src/components/Newsfeed/Newsfeed.js
+++ client/src/components/Newsfeed/Newsfeed.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { Component } from 'react';
 import "./Newsfeed.css";
+import Axios from "axios";
 
-export const Newsfeed = ({ children }) => {
-  return (
-    <div className="col s3 center card-panel">Newsfeed</div>
-  );
-};
+class Newsfeed extends Component {
+	state = {
+		news: []
+	}
+	componentDidMount() {
+		Axios.get("/api/news/scrape")
+			.then(res => {
+		}).catch(function(error) {
+                console.error(error);
+            });
+
+	}
+
+	render () {
+		return <div className="col s3 center card-panel">Newsfeed</div>
+	}
+}
 
 export default Newsfeed;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,30 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
+const Article = require("../../models/article.js");
+const request = require("request");
+const cheerio = require("cheerio");
 
-// Book routes
-router.use("/books", bookRoutes);
+router.get("/news/scrape", function(req, res){
+	request("https://www.reddit.com/r/boardgames/top/?sort=top&t=day", function(error, response, html) {
+		let $ = cheerio.load(html);
+		$("p.title").each(function(i, element) {
+			let article = new Article({
+								title: $(element).children("a").text(),
+								link: $(element).children("a").attr("data-href-url"),
+							})
+			article.save();
+		})
+	})
+})
+
+
+router.get("/news", function(req, res){
+	Article.find().exec(function(error, result){
+		console.log(result);
+	})
+})
+
+
+
+
 
 module.exports = router;
