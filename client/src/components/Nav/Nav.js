8d87ac25fc8658402b6189eae6bc81c101faaa32
sternj20 <jessesternmusic@gmail.com>
2017-09-23 00:17:37 -0700

    added react materialize npm package. added link to material icons cdn and jquery cdn in index.html file. created navbar component using react materialize package

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,8 +1,10 @@
 import React from "react";
+import {Navbar, NavItem} from "react-materialize"
 
 const Nav = () =>
-  <nav className="nav">
-    navbar
-  </nav>;
-
+	<Navbar brand='Board Game' right>
+		<NavItem href='get-started.html'>Your Games</NavItem>
+		<NavItem href='components.html'>Add New Game</NavItem>
+		<NavItem href='components.html'>Play game</NavItem>
+	</Navbar>
 export default Nav;
