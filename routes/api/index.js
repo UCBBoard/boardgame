d7602b9f5d5f8f06be6c13aebb7923e9ec9f3e76
Jon Berry <jonpber@gmail.com>
2017-09-25 13:19:57 -0700

    starting work on BGG scraper; it's currently working on the first layer but it has issues when it gets to the second part where it actually pulls info from the site

--- routes/api/index.js
+++ routes/api/index.js
@@ -22,14 +22,25 @@ router.get("/news/scrape", function(req, res){
 			res.json(result.slice(0,5))
 		}), 1000
 		})
-		
 	})
 })
 
+router.get("/games/:name", function(req, res){
+	request("https://boardgamegeek.com/geeksearch.php?action=search&objecttype=boardgame&B1=Go&q=" + req.params.name, function(error, response, html) {
+		let $ = cheerio.load(html);
+		$("#results_objectname1").each(function(i, element) {
+			let link = "https://boardgamegeek.com" + $(element).children("a").attr("href");
+			request(link, function(error1, response1, html1) {
+				let $1 = cheerio.load(html1);
+				$1(".game-header-body").each(function(i1, element1){
+					// let result = {
+					// 	name: element1.children(".game-header-title-container").children(".game-header-title").children(".game-header-title-info").children("h1").text()
+					// }
+					res.json("element1")
+				})
+			})
+		})
 
-router.get("/news", function(req, res){
-	Article.find().exec(function(error, result){
-		console.log(result);
 	})
 })
 
