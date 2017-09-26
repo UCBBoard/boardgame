703aa618c934d29616a8601daaba461291659566
sternj20 <jessesternmusic@gmail.com>
2017-09-25 17:51:00 -0700

    google auth is working with react router routing to dashboard automatically if a user has logged in. an issue is that the page is not automatically refreshing after user logs in, you have to manually refresh to get logged to the dash page. need to add logout button and save firebase id to a new user in db

--- routes/api/index.js
+++ routes/api/index.js
@@ -29,24 +29,7 @@ router.get("/news/scrape", function(req, res){
 	})
 })
 
-router.get("/games/:name", function(req, res){
-	request("https://boardgamegeek.com/geeksearch.php?action=search&objecttype=boardgame&B1=Go&q=" + req.params.name, function(error, response, html) {
-		let $ = cheerio.load(html);
-		$("#results_objectname1").each(function(i, element) {
-			let link = "https://boardgamegeek.com" + $(element).children("a").attr("href");
-			request(link, function(error1, response1, html1) {
-				let $1 = cheerio.load(html1);
-				$1(".game-header-body").each(function(i1, element1){
-					// let result = {
-					// 	name: element1.children(".game-header-title-container").children(".game-header-title").children(".game-header-title-info").children("h1").text()
-					// }
-					res.json("element1")
-				})
-			})
-		})
 
-<<<<<<< HEAD
-=======
 // For scraping from BGG to get boardgame info
 router.get("/games/:name", function(req, res){
 	request("https://boardgamegeek.com/geeksearch.php?action=search&objecttype=boardgame&B1=Go&q=" + req.params.name, function(error, response, html) {
@@ -64,7 +47,6 @@ router.get("/games/:name", function(req, res){
 			})
 		})
 
->>>>>>> upstream/master
 	})
 })
 
