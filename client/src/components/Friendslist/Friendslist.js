9bb57dac44c6832823cc00b2155cbb7fbfd9ec43
sternj20 <jessesternmusic@gmail.com>
2017-09-28 14:10:32 -0700

    added crude component to add two users to each others friends list

--- client/src/components/Friendslist/Friendslist.js
+++ client/src/components/Friendslist/Friendslist.js
@@ -1,9 +1,24 @@
 import React from "react";
 import "./Friendslist.css";
+import firebase from "firebase";
+import Axios from "axios";
+import {Button} from "react-materialize";
+
+const addFriend = () => {
+	let activeUser = firebase.auth().currentUser.uid
+	let secondUser = 33333333
+	let route = `/api/user/addfriend/${activeUser}/${secondUser}`
+	Axios.post(route, {
+		activeUser: activeUser,
+		secondUser: secondUser
+	})
+}
 
 export const Friendslist = ({ children }) => {
   return (
-    <div className="col s3 center card-panel">Friendslist</div>
+    <div className="col s3 center card-panel">Friendslist
+    	<Button onClick={addFriend}>Click me</Button>
+    </div>
   );
 };
 
