8ee32a24c4eebbd1387bebbdde864775c825f501
Jon Berry <jonpber@gmail.com>
2017-09-28 20:12:59 -0700

    added scrollbar to background component for different screen sizes; made loading component blank background

--- client/src/components/LoadingScreen/LoadingScreen.js
+++ client/src/components/LoadingScreen/LoadingScreen.js
@@ -5,7 +5,5 @@ import logo from "../../assets/img/logo.png"
 
 const LoadingScreen = () =>
 	<Background backgroundName="dash-background">
-		<img src={logo} className="loadingLogo" alt="logo" />
-		<h2>Loading</h2>
 	</Background>
 export default LoadingScreen;
