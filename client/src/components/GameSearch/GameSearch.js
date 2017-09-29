619483764a9554f6c5ba686f80f8fd11f3836a73
Jon Berry <jonpber@gmail.com>
2017-09-28 20:18:39 -0700

    fixed issue with scrollbar on dashboard

---
+++ client/src/components/GameSearch/GameSearch.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Background from "../Background"
+import "./GameSearch.css";
+
+
+const GameSearch = () =>
+	<Background backgroundName="dash-background">
+	</Background>
+export default GameSearch;
